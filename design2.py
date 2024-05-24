43370b7810fba5aabb246b78ca2aa4f151224840
Anjana2k02 <101281237+Anjana2k02@users.noreply.github.com>
2024-05-24 21:17:32 +0530

    v3

---
+++ design2.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.bgcolor('black')
+t = turtle.Turtle()
+colors = ['red', 'dark red']
+for number in range(400):
+    t.forward(number+1)
+    t.right(89)
+    t.pencolor(colors[number%2])
+turtle.done()
